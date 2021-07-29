36090c9c3a41e6c40efeafc2fc91322d4dff2c27
Vinayak <80584437+Vinayak2835@users.noreply.github.com>
2021-07-29 14:39:30 +0530

    [Vinayak] Add: Automation script for Excel sheet is added

--- src/test/java/com/salenium/ExcelUtils.java
+++ src/test/java/com/salenium/ExcelUtils.java
@@ -4,8 +4,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Iterator;
-
-
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
